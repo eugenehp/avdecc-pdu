54ea51b806cb973fe23434f82eb039c1d421a48e
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2011-11-30 10:09:59 -0800

    initial project layout

---
+++ project.mk
@@ -0,0 +1,18 @@
+PROJECT=avdecc-pdu
+PROJECT_NAME=avdecc-pdu
+PROJECT_VERSION=20111130
+PROJECT_EMAIL=jeff.koftinoff@ieee.org
+PROJECT_LICENSE=ISC
+PROJECT_MAINTAINER=jeff.koftinoff@ieee.org
+PROJECT_COPYRIGHT=Copyright 2011
+PROJECT_DESCRIPTION=avdecc-pdu
+PROJECT_WEBSITE=http://avdecc-pdu.statusbar.com/
+PROJECT_IDENTIFIER=com.statusbar.avdecc-pdu
+TOP_LIB_DIRS+=.
+CONFIG_TOOLS+=
+PKGCONFIG_PACKAGES+=
+
+COMPILE_FLAGS+=-Wall -Wunused -Wno-variadic-macros
+CFLAGS+=-std=c99
+
+
