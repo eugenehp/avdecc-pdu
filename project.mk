52b1743e0f6fb91c6db2f5fbcec2ba1da1b7b34e
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2011-12-04 15:52:21 -0800

    version bumped

--- project.mk
+++ project.mk
@@ -1,6 +1,6 @@
 PROJECT=avdecc-pdu
 PROJECT_NAME=avdecc-pdu
-PROJECT_VERSION=20111130
+PROJECT_VERSION=20121204
 PROJECT_EMAIL=jeff.koftinoff@ieee.org
 PROJECT_LICENSE=ISC
 PROJECT_MAINTAINER=Jeff Koftinoff <jeff.koftinoff@ieee.org>
